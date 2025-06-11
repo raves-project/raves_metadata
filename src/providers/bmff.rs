48e01ea767e6e51324b5bcc780510373e538b201
Barrett <onkoe@protonmail.com>
2025-06-11 14:20:28 -0500

    docs(prov/bmff): add link to BMFF explainer ppt

--- src/providers/bmff.rs
+++ src/providers/bmff.rs
@@ -1,2 +1,6 @@
 //! This module contains an IPTC parser for members of the ISO base media file
 //! format (ISOBMFF), or just "BMFF."
+//!
+//! BMFF contains information used for "timed" presentation of media data.[^1]
+//!
+//! [^1]: [An Overview of the ISO Base Media File Format by Thomas Stockhammer](https://www.youtube.com/watch?v=CLvR9FVYwWs?t=129)
