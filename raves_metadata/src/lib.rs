0624fec3c52008f2f302892602b9858f88ba2385
Barrett <onkoe@protonmail.com>
2025-06-11 19:55:38 -0500

    docs(lib.rs): reveal my evil plans
    ... in other words, add skeletons for Exif + XMP ;)

--- raves_metadata/src/lib.rs
+++ raves_metadata/src/lib.rs
@@ -15,11 +15,17 @@ pub struct Iptc {
     pub pairs: Pairs,
 }
 
-/// A media file with IPTC support.
+/// A media file with support for various metadata formats.
 ///
-/// Each file format is a "provider" - it'll yield its IPTC through parsing.
-pub trait IptcProvider {
+/// Each file format is a "provider" - it'll yield its metdata through parsing.
+pub trait MetadataProvider {
     /// Parses `self`, a media source, for its IPTC block(s) and returns them
     /// combined into one list of (key, value) pairs.
     fn iptc(&self) -> Result<Iptc, IptcError>;
+
+    // fn exif(&self) -> Result<Exif, ExifError>;
+
+    // fn xmp(&self) -> Result<Xmp, XmpError>;
 }
+
+pub enum IptcKey {}
