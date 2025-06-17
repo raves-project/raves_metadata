84a84e0e6f52087e638d7cdbd82cf74c1a9bf2c6
Barrett <onkoe@protonmail.com>
2025-06-16 19:36:41 -0500

    docs(meta/prov): quick explainer about "providers"

--- raves_metadata/src/providers/mod.rs
+++ raves_metadata/src/providers/mod.rs
@@ -1,1 +1,13 @@
+//! # Providers
+//!
+//! This module re-exports the many different "providers" used to obtain a
+//! media file's metadata.
+//!
+//! Providers represent a source of metadata, such as a media file format,
+//! including formats like JPEG, or a container format like ISO-BMFF.
+//!
+//! Each provider has a struct with a `MetadataProvider` implementation. This
+//! allows a consistent interface for obtaining metadata from different media
+//! sources.
+
 pub mod bmff;
