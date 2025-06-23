b8a1fce95a59ef92a2c194a7efd8833b10aa4034
Barrett <onkoe@protonmail.com>
2025-06-23 14:58:36 -0500

    test(meta/xmp/val/prims): add primitive tests

--- raves_metadata/src/xmp/value/prims.rs
+++ raves_metadata/src/xmp/value/prims.rs
@@ -59,3 +59,236 @@ pub fn parse_primitive<'xml>(text: Cow<'xml, str>, prim: &Prim) -> XmpValueResul
         Prim::Text => XmpValue::Simple(XmpPrimitive::Text(text)),
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use raves_metadata_types::{
+        xmp::{XmpPrimitive, XmpValue},
+        xmp_parsing_types::XmpPrimitiveKind as Prim,
+    };
+
+    use crate::xmp::{error::XmpParsingError, value::prims::parse_primitive};
+
+    /// Checks that booleans parse correctly, and that various incorrect
+    /// boolean representations error as expected.
+    #[test]
+    fn only_standard_compliant_booleans_should_parse() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // `True` and `False` should both parse correctly
+        assert_eq!(
+            parse_primitive("True".into(), &Prim::Boolean).expect("should parse"),
+            XmpValue::Simple(XmpPrimitive::Boolean(true)),
+            "`True` str is represented as `true` (`bool`) in Rust"
+        );
+        assert_eq!(
+            parse_primitive("False".into(), &Prim::Boolean).expect("should parse"),
+            XmpValue::Simple(XmpPrimitive::Boolean(false)),
+            "`False` str is represented as `false` (`bool`) in Rust"
+        );
+
+        // however, `true`, false`, `0`, `1`, and random text shouldn't
+        // magically become booleans lol
+        assert!(parse_primitive("true".into(), &Prim::Boolean).is_err());
+        assert!(parse_primitive("false".into(), &Prim::Boolean).is_err());
+        assert!(parse_primitive("1".into(), &Prim::Boolean).is_err());
+        assert!(parse_primitive("0".into(), &Prim::Boolean).is_err());
+        assert!(parse_primitive("random text".into(), &Prim::Boolean).is_err());
+    }
+
+    /// Ensures that smaller numbers parse into `i64`.
+    #[test]
+    fn normal_numbers_parse() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // small pos. number
+        assert_eq!(
+            parse_primitive("2025".into(), &Prim::Integer).expect("small values should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(2025_i64)),
+        );
+
+        // small-ish neg. number
+        assert_eq!(
+            parse_primitive("-2147483648".into(), &Prim::Integer).expect("`i32::MIN` should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(i32::MIN.into())),
+        );
+
+        // zero
+        assert_eq!(
+            parse_primitive("0".into(), &Prim::Integer).expect("positive zero should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(0_i64)),
+        );
+
+        // negative zero
+        assert_eq!(
+            parse_primitive("-0".into(), &Prim::Integer).expect("negative zero should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(0_i64)),
+        );
+
+        // both `i64::MIN` and `i64::MAX` should parse
+        assert_eq!(
+            parse_primitive("-9223372036854775808".into(), &Prim::Integer)
+                .expect("`i64::MIN` should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(i64::MIN)),
+        );
+        assert_eq!(
+            parse_primitive("9223372036854775807".into(), &Prim::Integer)
+                .expect("`i64::MAX` should parse"),
+            XmpValue::Simple(XmpPrimitive::Integer(i64::MAX)),
+        );
+
+        // a single `+` or `-` sign may precede the value
+        assert_eq!(
+            parse_primitive("+1".into(), &Prim::Integer)
+                .expect("plus sign is permitted before an int"),
+            XmpValue::Simple(XmpPrimitive::Integer(1_i64)),
+        );
+        assert_eq!(
+            parse_primitive("-1".into(), &Prim::Integer)
+                .expect("minus sign is permitted before an int"),
+            XmpValue::Simple(XmpPrimitive::Integer(-1_i64)),
+        );
+    }
+
+    /// Ensures that huge numbers become `Prim::Text` instead of failing.
+    #[test]
+    fn huge_numbers_parse_as_prim_text() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // giant numbers can be parsed as `Prim::Text`
+        const GIANT_NUMBER_STR: &str = "9857203947509273094750927345702738904578927308945789023475";
+        assert_eq!(
+            parse_primitive(GIANT_NUMBER_STR.into(), &Prim::Integer).expect("should parse as str"),
+            XmpValue::Simple(XmpPrimitive::Text(GIANT_NUMBER_STR.into())),
+        );
+
+        // however, those with other problems shouldn't be so lucky...
+        const FAILING_NUMBER_STR: &str = "1.2.3";
+        assert!(
+            matches!(
+                parse_primitive(FAILING_NUMBER_STR.into(), &Prim::Integer),
+                Err(XmpParsingError::PrimitiveIntegerParseFail(
+                    std::borrow::Cow::Borrowed("1.2.3"),
+                    _
+                ))
+            ),
+            "legitimately malformed ints should error like normal"
+        );
+    }
+
+    /// Ensures that any text can be represented as a `Prim::Date`.
+    ///
+    /// Currently, we just parse dates as raw text without any actual
+    /// verification.
+    ///
+    /// If you adjust that, you'll also need to adjust this test to confirm
+    /// you've intended to change that.
+    #[test]
+    fn dates_are_just_text() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // correctly-formatted date should work fine
+        const CORRECTLY_FORMATTED_DATE: &str = "2025-06-23T14:33:00-06:00";
+        assert_eq!(
+            parse_primitive(CORRECTLY_FORMATTED_DATE.into(), &Prim::Date)
+                .expect("a correct date should parse just fine..."),
+            XmpValue::Simple(XmpPrimitive::Date(CORRECTLY_FORMATTED_DATE.into())),
+        );
+
+        // but so should some random text
+        const OBVIOUSLY_NOT_A_DATE_STR: &str = "not a date lol";
+        assert_eq!(
+            parse_primitive(OBVIOUSLY_NOT_A_DATE_STR.into(), &Prim::Date)
+                .expect("random txt should parse"),
+            XmpValue::Simple(XmpPrimitive::Date(OBVIOUSLY_NOT_A_DATE_STR.into())),
+        );
+    }
+
+    /// Checks that floats (Reals) parse as expected.
+    #[test]
+    fn reals_should_parse() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // first, `<xmp:Rating>` often contains just integers, despite being a
+        // float type.
+        //
+        // we'll try those to ensure they'll parse
+        assert_eq!(
+            parse_primitive("1".into(), &Prim::Real).expect("ints should parse as a real"),
+            XmpValue::Simple(XmpPrimitive::Real(1_f64)),
+        );
+
+        // Rust-style `0.` (i.e. optional last value) should work just fine.
+        assert_eq!(
+            parse_primitive("0.".into(), &Prim::Real).expect("decimal places should be optional"),
+            XmpValue::Simple(XmpPrimitive::Real(0_f64)),
+        );
+
+        // large floats can parse, too.
+        //
+        // they might not be 100% accurate when serialized, though!
+        assert_eq!(
+            parse_primitive(
+                "100000000000000000.000000000000000000001".into(),
+                &Prim::Real
+            )
+            .expect("decimal places should be optional"),
+            XmpValue::Simple(XmpPrimitive::Real(100000000000000000_f64)),
+        );
+
+        // a single `+` or `-` sign may precede the value
+        assert_eq!(
+            parse_primitive("+1.0".into(), &Prim::Real)
+                .expect("plus sign is permitted before a real"),
+            XmpValue::Simple(XmpPrimitive::Real(1.0_f64)),
+        );
+        assert_eq!(
+            parse_primitive("-1.0".into(), &Prim::Real)
+                .expect("minus sign is permitted before a real"),
+            XmpValue::Simple(XmpPrimitive::Real(-1.0_f64)),
+        );
+        assert_eq!(
+            parse_primitive("1.0".into(), &Prim::Real).expect("signs are not required"),
+            XmpValue::Simple(XmpPrimitive::Real(1.0_f64)),
+        );
+    }
+
+    /// Text should parse.
+    #[test]
+    fn text_should_parse() {
+        _ = env_logger::builder()
+            .filter_level(log::LevelFilter::max())
+            .format_file(true)
+            .format_line_number(true)
+            .try_init();
+
+        // stuck in some random text w/ different scripts.
+        //
+        // parser should handle it just fine ;D
+        const SOME_TEXT: &str = "see you later, alligator! بعد فترة، تمساح";
+        assert_eq!(
+            parse_primitive(SOME_TEXT.into(), &Prim::Text).expect("random txt should parse"),
+            XmpValue::Simple(XmpPrimitive::Text(SOME_TEXT.into())),
+        );
+    }
+}
