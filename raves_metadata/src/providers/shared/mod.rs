7261ee40441e64ffd8f14659c42028793b0f7dcc
Barrett <onkoe@protonmail.com>
2025-09-15 12:32:06 -0500

    feat(meta/shared): add NUL-terminated str parser

--- raves_metadata/src/providers/shared/mod.rs
+++ raves_metadata/src/providers/shared/mod.rs
@@ -7,8 +7,13 @@
 //!
 //! Instead, it's shared here!
 
+use winnow::error::ContextError;
+use winnow::token::literal;
+use winnow::{ModalResult, Parser as _};
 use winnow::{
+    combinator::terminated,
     error::{StrContext, StrContextValue},
+    token::take_until,
 };
 
 pub mod bmff;
@@ -16,4 +21,18 @@ pub mod bmff;
 /// Creates a string description for `winnow` context.
 pub const fn desc(s: &'static str) -> StrContext {
     StrContext::Expected(StrContextValue::Description(s))
+}
+
+/// Parses out a NUL-terminated string.
+///
+/// Consumes the NUL.
+pub fn parse_nul_terminated_str<'input>(
+    input: &mut &'input [u8],
+) -> ModalResult<&'input str, ContextError> {
+    terminated(
+        take_until(0.., "\0").context(desc("NUL-terminated string")),
+        literal("\0").context(desc("NUL literal")),
+    )
+    .try_map(std::str::from_utf8)
+    .parse_next(input)
 }
