6ef61605682682fbd4e6eac4ea2e29d8dac37c7b
Barrett <onkoe@protonmail.com>
2025-09-15 13:55:06 -0500

    test(meta/heic): add basic nokia heif conform test

--- raves_metadata/src/providers/heic.rs
+++ raves_metadata/src/providers/heic.rs
@@ -40,3 +40,60 @@ impl<'input> MetadataProvider for Heic<'input> {
         self.heic_like.xmp()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use raves_metadata_types::exif::{
+        Field, FieldData, FieldTag,
+        primitives::{Primitive, PrimitiveTy},
+        tags::{ExifIfdTag, KnownTag},
+    };
+
+    use crate::{MetadataProvider, exif::Exif, providers::heic::Heic, util::logger};
+
+    #[test]
+    fn nokia_conformance_file_c034_heic_parses_despite_malformed_exif_tiff_header_offset() {
+        logger();
+
+        let blob: &[u8] = include_bytes!("../../assets/providers/heic/C034.heic");
+
+        // parse it into heic
+        let file: Heic = Heic::new(blob).expect("parse as heic");
+
+        // it should only have exif
+        assert!(file.iptc().is_none(), "iptc unsupported");
+        assert!(file.xmp().is_none(), "xmp not present in file");
+
+        // grab exif
+        let exif: Exif = file
+            .exif()
+            .expect("file has exif")
+            .expect("exif should be well-formed");
+
+        // grab its exif ifd
+        let exif_ifd = exif
+            .ifds
+            .first()
+            .expect("should have an ifd")
+            .sub_ifds
+            .first()
+            .expect("should have one sub-ifd, which is ExifIFD");
+
+        // ensure its "exif version" is 0230
+        assert_eq!(
+            *exif_ifd
+                .fields
+                .iter()
+                .flatten()
+                .find(|f| f.tag == FieldTag::Known(KnownTag::ExifIfdTag(ExifIfdTag::ExifVersion)))
+                .expect("find ExifVersion field"),
+            Field {
+                tag: FieldTag::Known(KnownTag::ExifIfdTag(ExifIfdTag::ExifVersion)),
+                data: FieldData::List {
+                    list: [0x30, 0x32, 0x33, 0x30].map(Primitive::Undefined).into(),
+                    ty: PrimitiveTy::Undefined
+                }
+            }
+        );
+    }
+}
