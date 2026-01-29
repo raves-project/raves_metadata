0de2ade0da2272db77c33d0c3a1dd9dd74498af9
Barrett <onkoe@protonmail.com>
2026-01-28 23:15:03 -0600

    feat(meta/gif): implement display for gif error

--- raves_metadata/src/providers/gif/error.rs
+++ raves_metadata/src/providers/gif/error.rs
@@ -112,6 +112,89 @@ impl core::error::Error for GifConstructionError {}
 
 impl core::fmt::Display for GifConstructionError {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        let t = todo!();
+        match self {
+            GifConstructionError::NoMagicNumber => {
+                f.write_str("Attempted to parse out magic number, but none was present.")
+            }
+            GifConstructionError::WeirdMagicNumber(found) => write!(
+                f,
+                "The given file was not a GIF. \
+                First bytes should be `[G, I, F]`, but got: `{found:x?}`!",
+            ),
+            GifConstructionError::NoGifVersion => f.write_str(
+                "There weren't enough bytes in the stream to get the \
+                GIF version for the file's header.",
+            ),
+            GifConstructionError::LogicalScreenDescriptorMissingData => {
+                f.write_str("The GIF did not contain the required logical screen descriptor.")
+            }
+            GifConstructionError::GlobalColorTableMissingTriplet {
+                expected_triplet_ct,
+                errant_triplet,
+                missing_color,
+            } => write!(
+                f,
+                "The global color table is missing a triplet. \
+                Errant triplet was {errant_triplet}/{expected_triplet_ct} \
+                on color {missing_color:?}."
+            ),
+            GifConstructionError::UnknownBlockFound { byte } => write!(
+                f,
+                "Unknown block found when parsing repeatable blocks. \
+                    Block's first byte: `0x{byte:x}`",
+            ),
+            GifConstructionError::UnknownExtensionFound { label } => write!(
+                f,
+                "Unknown extension found when parsing repeatable blocks. \
+                    Extension's label: `0x{label:x}`",
+            ),
+            GifConstructionError::ExtensionHasWeirdBlockSize { got, expected } => {
+                write!(
+                    f,
+                    "Extension had an unexpected block size! \
+                    Got: {got} bytes, but expected: {expected} bytes."
+                )
+            }
+            GifConstructionError::ExtensionFoundInGif87 => f.write_str(
+                "A GIF of version 87a contained an extension, which violates the standard.",
+            ),
+            GifConstructionError::NotEnoughBytes => {
+                f.write_str("Parser expected more bytes, but the input ran out of data.")
+            }
+            GifConstructionError::BlockTerminatorMismatch(got) => {
+                write!(
+                    f,
+                    "Expected block terminator (`0x00`), but found: `0x{got:x}`."
+                )
+            }
+            GifConstructionError::ImageDescriptorNoSeparator => {
+                f.write_str("An image descriptor had no separator.")
+            }
+            GifConstructionError::ImageDescriptorSeparatorWrong(got) => write!(
+                f,
+                "Expected separator in image descriptor (`0x2c`), but found: `0x{got:x}`."
+            ),
+            GifConstructionError::ImageDescriptorMissingData => {
+                f.write_str("An image descriptor was missing required fields.")
+            }
+            GifConstructionError::TableBasedImageDataNoLzw => {
+                f.write_str("Table-based image data is missing its LZW size field.")
+            }
+            GifConstructionError::GraphicExtMissingData => {
+                f.write_str("Graphic control extension is missing data.")
+            }
+            GifConstructionError::CommentExtMissingData => {
+                f.write_str("Comment extension is missing data.")
+            }
+            GifConstructionError::AppExtMissingData => {
+                f.write_str("Application extension is missing data.")
+            }
+            GifConstructionError::PlainTextExtMissingData => {
+                f.write_str("Plain-text extension is missing data.")
+            }
+            GifConstructionError::TrailerMissing => f.write_str(
+                "The provided GIF file abrupted stopped without its required trailer (end) block.",
+            ),
+        }
     }
 }
